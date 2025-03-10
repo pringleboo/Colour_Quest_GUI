c1da8962e76f5297b2e4f5b9cca6a81eb4e309a6
pringleboo <101750172+pringleboo@users.noreply.github.com>
2025-03-11 10:05:43 +1300

    unfinished video 20

--- C_02_Game_Component_v2.py
+++ C_02_Game_Component_v2.py
@@ -107,6 +107,22 @@ class Play:
     """
 
     def __init__(self, how_many):
+
+        # Integers / String Variables
+        self.target_score - IntVar()
+
+        # Rounds played - start with zero
+        self.rounds_played = IntVar()
+        self.rounds_played.set(0)
+
+        self.rounds_wanted = IntVar()
+        self.rounds_wanted.set(how_many)
+
+        # Colour lists and score list
+        self.round_colour_list = []
+        self.all_scores_list = []
+        self.all_medians_list = []
+
         self.play_box = Toplevel()
 
         self.game_frame = Frame(self.play_box)
@@ -129,7 +145,7 @@ class Play:
                                     bg=item[2], wraplength=300, justify="left")
             self.make_label.grid(row=item[3], pady=10, padx=10)
 
-            play_labels_ref.append(item)
+            play_labels_ref.append(self.make_label)
 
         # Retrieve Labels so they can be configured later
         self.heading_label = play_labels_ref[0]
@@ -169,6 +185,42 @@ class Play:
                 make_control_button.grid(row=item[5], column=item[6], padx=5, pady=5)
 
                 control_ref_list.append(make_control_button)
+
+            # Once interface has been created, invoke new
+            # round function for first round.
+            self.new_round()
+
+        # Set target score as median (for later comparison)
+        self.target_score.set(median)
+
+        # Update heading, and score to beat labels. "Hide" results label
+        self.heading_label.config(text=f"Round {rounds_played} of {rounds_wanted}")
+        self.target_label.config(text=f"Target Score: {median}",
+                                 font=("Arial", "14", "bold"))
+        self.results_label.config(text=f"{'=' * 7}", bg="#F0F0F0")
+
+        # Configure buttons using foreground and background colours from list
+        # Enable colour buttons (disabled at the end of the last round)
+        for count, item in enumerate(self.colour_button_ref):
+            item.config(fg=self.round_colour_list[count][2],
+                        bg=self.round_colour_list[count][0],
+                        text=self.round
+
+    def new_round(self):
+        """
+        Chooses four colours, works out median for score to beat. Configures
+        buttons with chosen colours
+        """
+
+        # Retrieve number of rounds played, add one to it and configure heading
+        rounds_played = self.rounds_played.get()
+        rounds_played += 1
+        self.rounds_played.set(rounds_played)
+
+        rounds_wanted = self.rounds_wanted.get()
+
+        # Get round colours and median score...
+        self.round_colour_list, median = get_round_colours()
 
     def close_play(self):
         # Reshow root (ie: choose rounds) and end current

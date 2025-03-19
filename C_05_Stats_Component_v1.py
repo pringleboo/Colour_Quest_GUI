2da2bb106a554af08fc1ab6ca32bd20f673b6e47
pringleboo <101750172+pringleboo@users.noreply.github.com>
2025-03-20 11:04:00 +1300

    Partway through video 24

---
+++ C_05_Stats_Component_v1.py
@@ -0,0 +1,229 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+
+class StartGame:
+    """
+    Initial Game interface (asks users how many rounds they
+    would like to play)
+    """
+
+    def __init__(self):
+        """
+        Gets number of rounds from user
+        """
+
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Create play button...
+        self.play_button = Button(self.start_frame, font=("Arial", "16", "bold"),
+                                  fg="#FFFFFF", bg="#0057D8", text="Play", width=10,
+                                  command=self.check_rounds)
+        self.play_button.grid(row=0, column=1, padx=20, pady=20)
+
+    def check_rounds(self):
+        """
+        Checks user have entered 1 or more rounds
+        """
+
+        # Retrieve temperature to be converted
+        rounds_wanted = 5
+        self.to_play(rounds_wanted)
+
+    def to_play(self, num_rounds):
+        """
+        Invokes Game GUI and takes across number of rounds to be played.
+        """
+        Play(num_rounds)
+        # Hide root window (ie: hide rounds choice window).
+        root.withdraw()
+
+
+class Play:
+    """
+    Interface for playing the Colour Quest Game
+    """
+
+    def __init__(self, how_many):
+        self.rounds_won = IntVar()
+
+        # List for stats component
+
+        # # Highest Score Test Data...
+        # self.all_scores_list = [20, 20, 20, 16, 19]
+        # self.all_high_score_list = [20, 20, 20, 16, 19]
+        # self.rounds_won.set(5)
+        #
+        # # Lowest Score Test Data...
+        # self.all_scores_list = [0, 0, 0, 0, 0]
+        # self.all_high_score_list = [20, 20, 20, 16, 19]
+        # self.rounds_won.set(0)
+        #
+        # # Random Score Test Data...
+        # self.all_scores_list = [0, 15, 16, 0, 16]
+        # self.all_high_score_list = [20, 19, 18, 20, 20]
+        # self.rounds_won.set(3)
+
+        self.play_box = Toplevel()
+
+        self.game_frame = Frame(self.play_box)
+        self.game_frame.grid(padx=10, pady=10)
+
+        self.heading_label = Label(self.game_frame, text="Colour Quest", font=("Arial", "16", "bold"),
+                                   padx=5, pady=5)
+        self.heading_label.grid(row=0)
+
+        self.stats_button = Button(self.game_frame, font=("Arial", "14", "bold"),
+                                   text="Stats", width=15, fg="#FFFFFF",
+                                   bg="#FF8000", padx=10, pady=10, command=self.to_stats)
+        self.stats_button.grid(row=1)
+
+    def to_stats(self):
+        """
+        Retrieves everything we need to display the game / round statistics
+        """
+
+        # IMPORTANT: Retrieve number of rounds
+        # won as a number (rather than the 'self' container)
+        rounds_won = self.rounds_won.get()
+        stats_bundle = [rounds_won, self.all_scores_list,
+                        self.all_high_scores_list]
+        Stats(self, stats_bundle)
+
+
+class Stats:
+    """
+    Displays stats for Colour Quest Game
+    """
+
+    def __init__(self, partner, all_stats_info):
+
+        # Extract information from master list...
+        rounds_won = all_stats_info[0]
+        user_scores = all_stats_info[1]
+        high_scores = all_stats_info[2]
+
+        # Sort user scores to find score...
+        user_scores.sort()
+
+        # setup dialogue box and background colour
+        self.stats_box = Toplevel()
+
+        # Disable stats button
+        partner.stats_button.config(state=DISABLED)
+
+        # If users press cross at top, closes stats and
+        # 'releases' stats button
+        self.stats_box.protocol('WM_DELETE_WINDOW',
+                                partial(self.close_stats, partner))
+
+        self.stats_frame = Frame(self.stats_box, width=300,
+                                 height=200)
+        self.stats_frame.grid()
+
+        # Math to populate Stats dialogue...
+        rounds_played = len(user_scores)
+
+        success_rate = rounds_won / rounds_played * 100
+        total_score = sum(user_scores)
+        max_possible = sum(high_scores)
+
+        best_score = user_scores[-1]
+        average_score = total_score / rounds_played
+
+        # Strings for Stats labels...
+
+        success_string = (f"Success Rate: {rounds_won} / {rounds_played}"
+                          f" ({success_rate:.0f}%")
+        total_score_string = f"Total Score: {total_score}"
+        max_possible_string = f"Maximum Possible Score: {max_possible}"
+        best_score_string = f"Best Score: {best_score}"
+
+        # Custom comment text and formatting
+        if total_score == max_possible:
+            comment_string = "Amazing! You got the highest possible score!"
+            comment_colour = "#D5E8D4"
+
+        elif total_score == 0:
+            comment_string = ("Oops - You've lost every round!"
+                              "You might want to look at the hints!")
+            comment_colour = "#F8CECC"
+            best_score_string = f"Best Score: n/a"
+        else:
+            comment_string = ""
+            comment_colour = "#F0F0F0"
+
+        average_score_string = f"Average Score: {average_score:.0f}\n"
+
+        heading_font = ("Arial", "16", "bold")
+        normal_font = ("Arial", "14")
+        comment_font = ("Arial", "13")
+
+        # Label list (text | font | 'Sticky')
+        all_stats_strings = [
+            ["Statistics", heading_font, ""],
+            [success_string, normal_font, "W"],
+            [total_score_string, normal_font, "W"],
+            [max_possible_string, normal_font, "W"],
+            [comment_string, comment_font, "W"],
+            ["\nRound Stats", normal_font, ""],
+            [best_score_string, normal_font, "W"],
+            [average_score_string, normal_font, "W"],
+        ]
+
+        stats_label_ref_list = []
+        for count, item in enumerate(all_stats_strings):
+            self.stats_label = Label(self.stats_frame, text=item[0], font=item[1],
+                                     anchor="w", justify="left",
+                                     padx=30, pady=5)
+            self.stats_label.grid(row=count, sticky=item[2], padx=10)
+            stats_label_ref_list.append(self.stats_label)
+
+        # Configure comment label background (for all won / all lost)
+        stats_comment_label = stats_label_ref_list[4]
+        stats_comment_label.config(bg=comment_colour)
+
+
+
+        self.stats_heading_label = Label(self.stats_frame,
+                                         text="Stats",
+                                         font=("Arial", "14", "bold"))
+        self.stats_heading_label.grid(row=0)
+
+        stats_text = "STATS GO HERE"
+
+        self.stats_text_label = Label(self.stats_frame,
+                                      text=stats_text, wraplength=350,
+                                      justify="left")
+        self.stats_text_label.grid(row=1, padx=10)
+
+        self.dismiss_button = Button(self.stats_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_stats, partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+        # List and loop to set background colour on everything except the buttons
+        recolour_list = [self.stats_frame, self.stats_heading_label, self.stats_text_label]
+
+        for item in recolour_list:
+            item.config(bg=background)
+
+    def close_stats(self, partner):
+        """
+        Closes stats dialogue box (and enables stats button)
+        """
+        # Put stats button back to normal...
+        partner.stats_button.config(state=NORMAL)
+        self.stats_box.destroy()
+
+
+# Main Routine
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    StartGame()
+    root.mainloop()
